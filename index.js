bb0f109af5908a254924bcdf39a4b4d35b3028fb
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-30 19:12:52 -0700

    added ability to add new employees and new roles

--- index.js
+++ index.js
@@ -15,19 +15,20 @@ const connection = mysql.createConnection({
       name: "task",
       message: "Would you like to do?",
       choices: [
-        "View Employees",
+        "View All Employees",
         "Add Employee",
         "Update Employee Role",
         "View All Roles",
         "Add Role",
         "View All Deparments",
         "Add Department",
+        "Update Employee Role",
         "End"]
     })
     .then(function(answer){
         console.log(answer)
-        if (answer.task === "View Employees"){
-          connection.query("SELECT * FROM employee", function(err, result){
+        if (answer.task === "View All Employees"){
+          connection.query("SELECT * FROM employee INNER JOIN role ON employee.role_id = role.id", function(err, result){
             if (err) throw err;
             console.table(result)
             runProgram()
@@ -62,18 +63,65 @@ const connection = mysql.createConnection({
             })
           })
         }
-        if (answer.task === "Add Employee"){
+        if (answer.task === "Add Role"){
           inquirer
-          .prompt({
-            type: "input",
-            name: "name",
-            message: "What is the employee info?",
-          })
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "What is the role title?",
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "What is the role salary?",
+            },
+            {
+              type: "list",
+              name: "department_id",
+              message: "What is the department ID?",
+              choices: ["1", "2", "3", "4"] 
+            }
+          ])
           .then(function(data){
-            connection.query(`INSERT INTO employee (title, salary, department_id) VALUES ("${data.title}", ${data.salary}, ${data.department_id})`, function(err, result){
+            connection.query(`INSERT INTO role (title, salary, department_id) VALUES ("${data.title}", ${data.salary}, ${data.department_id})`, function(err, result){
               if (err) throw err;
             console.table(result)
             runProgram()
+            })
+          })
+        }
+        if (answer.task === "Add Employee"){
+          inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "first_name",
+              message: "What is their fist name?",
+            },
+            {
+              type: "input",
+              name: "last_name",
+              message: "What is their last name?",
+            },
+            {
+              type: "list",
+              name: "role_id",
+              message: "What is their role ?",
+              choices: ["1", "2", "3", "4", "5", "6", "7", "8"] 
+            },
+            {
+              type: "list",
+              name: "manager_id",
+              message: "What is the manager ID?",
+              choices: ["1", "2", "3", "4"] 
+            }
+          ])
+          .then(function(data){
+            connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${data.first_name}", "${data.last_name}", ${data.role_id}, ${data.manager_id})`, function(err, result){
+              if (err) throw err;
+              console.table(result)
+              runProgram()
             })
           })
         }
